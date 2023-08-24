cc1e362d8d1562616b472e34c8b5cef4e67d27ec
Akash <vaishnavaksh@gmail.com>
2023-08-24 11:49:52 +0530

    first chat app

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+const socketIO = require('socket.io');
+
+const io = socketIO(server, {
+  cors: {
+    origin: 'http://127.0.0.1:5500', 
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true
+  }
+});
+const users = {};
+io.on("connection", (socket) => {
+  socket.on('new-user-connect',(name)=>{
+    users[socket.id] = name;
+    
+    console.log('user connect ' + name);
+    socket.broadcast.emit('user-joined',name);
+  })
+
+  socket.on('send',(message)=>{
+    socket.broadcast.emit('receive',{message:message,name:users[socket.id]});
+  })
+
+
+  socket.on('disconnect', message =>{
+    socket.broadcast.emit('left',users[socket.id]);
+    delete users[socket.id];
+});
+});
+
+
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
